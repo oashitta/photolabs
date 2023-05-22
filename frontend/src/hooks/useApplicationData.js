deead02c09b0f69364d13c9a1468a311b7e8c25a
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-05-22 19:59:34 -0400

    implementing useReducer hook

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,57 +1,84 @@
-import { useState, useReducer } from "react";
+import { useReducer } from "react";
 // takes props from App.jsx
+
+const initialState = {
+  favPhotos: [],
+  openModal: false,
+  clickedPhoto: null,
+};
 
 export const ACTIONS = {
   OPEN_MODAL: "OPEN_MODAL",
   CLOSE_MODAL: "CLOSE_MODAL",
   TOGGLE_FAVORITE: "TOGGLE_FAVORITE",
-  ADD_FAV_PHOTO: "ADD_FAV_PHOTO",
   CLICKED_PHOTO: "CLICKED_PHOTO",
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case OPEN_MODAL:
+    case ACTIONS.OPEN_MODAL:
       return { ...state, openModal: true, clickedPhoto: action.photoProps };
+
+    case ACTIONS.CLOSE_MODAL:
+      return {
+        ...state,
+        openModal: false,
+        clickedPhoto: null,
+      };
+
+    case ACTIONS.TOGGLE_FAVORITE:
+      const { photoId } = action.payload;
+      const isFavorited = state.favPhotos.includes(photoId);
+
+      if (isFavorited) {
+        return {
+          ...state,
+          favPhotos: state.favPhotos.filter((id) => id !== photoId),
+        };
+      } else {
+        return {
+          ...state,
+          favPhotos: [...state.favPhotos, photoId],
+        };
+      }
+    case ACTIONS.CLICKED_PHOTO:
+      return {
+        ...state,
+        clickedPhoto: action.photoProps,
+      };
     default:
       throw new Error(
         `Tried to reduce with unsupported action type: ${action.type}`
       );
   }
 }
+
 const useApplicationData = () => {
-  // state to keep track of the details of the photos favorited.
-  const [favPhotos, setFavPhotos] = useState([]);
-  // manages modals active state
-  const [openModal, setOpenModal] = useState(false);
-  // manages photo data that will be passed to modal
-  const [clickedPhoto, setClickedPhoto] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const toggleFavorite = (photoId) => {
-    if (favPhotos.includes(photoId)) {
-      setFavPhotos(favPhotos.filter((id) => id !== photoId));
-    } else {
-      setFavPhotos((prev) => [...prev, photoId]);
-    }
+    dispatch({ type: ACTIONS.TOGGLE_FAVORITE, payload: { photoId } });
   };
 
-  // to open the modal
-  const photoClickHandler = (photoProps) => {
-    setClickedPhoto(photoProps);
-    setOpenModal(true);
+  const openModal = (photoProps) => {
+    dispatch({ type: ACTIONS.OPEN_MODAL, photoProps });
   };
 
   const closeModal = () => {
-    setOpenModal(false);
-    setClickedPhoto(null);
+    dispatch({ type: ACTIONS.CLOSE_MODAL });
+  };
+  // to open the modal
+  const photoClickHandler = (photoProps) => {
+    dispatch({ type: ACTIONS.CLICKED_PHOTO, photoProps });
+    openModal(photoProps);
   };
 
   return {
-    favPhotos,
+    favPhotos: state.favPhotos,
     toggleFavorite,
-    openModal,
+    openModal: state.openModal,
     photoClickHandler,
-    clickedPhoto,
+    clickedPhoto: state.clickedPhoto,
     closeModal,
   };
 };
