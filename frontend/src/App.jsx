8306d2ce55232ee7f08148bdf1f1e7c1dad0ee03
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-05-23 18:49:40 -0400

    adding useEffect to fetch photo and topics data from api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import useApplicationData from "./hooks/useApplicationData";
 import HomeRoute from "./routes/HomeRoute";
-import photos from "./mocks/photos.json";
-import topics from "./mocks/topics.json";
 import "./App.scss";
 
 // Note: Rendering a single component to build components in isolation
@@ -16,6 +14,27 @@ const App = (props) => {
     clickedPhoto,
     closeModal,
   } = useApplicationData();
+
+  // photo and topics data now come from the appplication and the state is being tracked using the useStateHook.
+  const [photos, setPhotos] = useState([]);
+  const [topics, setTopics] = useState([]);
+
+  //Creating an effect to make a GET request to /api/photos and /api/topics using fetch and updating the photos and topics state with the response. No longer using hard coded data to render photos and topics.
+  useEffect(() => {
+    fetch("http://localhost:8001/api/photos")
+      .then((res) => res.json())
+      .then((data) => {
+        setPhotos([...data]);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:8001/api/topics")
+      .then((res) => res.json())
+      .then((data) => {
+        setTopics([...data]);
+      });
+  }, []);
 
   return (
     <div className="App">
